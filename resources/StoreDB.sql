f7e9a4c215d7e8d13d43bc9f848fde0983c1ec4d
MQapany <58705135+MQapany@users.noreply.github.com>
2020-02-21 18:01:11 +0200

    updates to be StoreBE

--- resources/StoreDB.sql
+++ resources/StoreDB.sql
@@ -1,3 +1,5 @@
+create database store;
+ 
 create table products (productId INT unsigned NOT NULL AUTO_INCREMENT,
 productName VARCHAR(150) NOT NULL,
 productDescription text,
@@ -6,4 +8,4 @@ price decimal(10,2) NOT NULL,
 productImage BLOB,
 PRIMARY KEY (productId));
 
-insert into 
+insert into products (productName, productDescription,quantityInStock,price,productImage) values ('testProduct','First product for testing',1000,100.50,LOAD_FILE('C:/Users/Moham/Downloads/food-samples.jpg'));
